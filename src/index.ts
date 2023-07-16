37bd108c985210dab8cdd4889b2a84f538cafae6
Po Co <polniolino@gmail.com>
2023-07-16 21:26:01 +0200

    Add main file to execute project

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-import lex from "./lex";
+import { readFileSync } from "fs";
+import interprete from "./runtime/interprete";
 
-console.log("Hello, World!");
-console.log(lex("let x = 1;"));
+const filename = process.argv[2];
+if (filename === undefined) {
+  console.error("Please provide a filename");
+  process.exit(1);
+}
+
+const code = readFileSync(filename, "utf-8");
+interprete(code);
